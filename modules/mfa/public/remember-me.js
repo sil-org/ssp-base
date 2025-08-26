cd07519c1900fd3362da1dd303e93909974fdfdd
Michael Wilson <michael_wilson@sil.org>
2025-08-26 11:06:35 +0930

    desiredRememberMeState should be set in local storage if not set

--- modules/mfa/public/remember-me.js
+++ modules/mfa/public/remember-me.js
@@ -1,10 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
   const rememberMeCheckbox = document.getElementById('rememberMe');
-  
   if (rememberMeCheckbox) {
     const desiredRememberMeState = localStorage.getItem('desiredRememberMeState');
+
+    if (desiredRememberMeState === null) {
+      localStorage.setItem('desiredRememberMeState', rememberMeCheckbox.checked);
+    }
+
     if (desiredRememberMeState === 'true') {
       rememberMeCheckbox.checked = true;
+    }
+
+    if (desiredRememberMeState === 'false') {
+      rememberMeCheckbox.checked = false;
     }
 
     rememberMeCheckbox.addEventListener('change', function() {
