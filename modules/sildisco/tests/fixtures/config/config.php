cb60cd49b2196daa6eb0589ff490317956adef9e
Devon <122382412+devon-sil@users.noreply.github.com>
2026-01-30 14:58:06 -0500

    Enable other modules in sildisco test config
    admin, saml, expirychecker, material, mfa, profilereview, silauth

--- modules/sildisco/tests/fixtures/config/config.php
+++ modules/sildisco/tests/fixtures/config/config.php
@@ -3,6 +3,13 @@
 $config = [
     'module.enable' => [
         'core' => true,
+        'admin' => true,
+        'saml' => true,
+        'expirychecker' => true,
+        'material' => true,
+        'mfa' => true,
+        'profilereview' => true,
+        'silauth' => true,
         'sildisco' => true,
     ],
     'metadatadir' => __DIR__ . '/../metadata',
