e6648948bb9be70e868055dcde896283471a98c5
briskt <3172830+briskt@users.noreply.github.com>
2026-04-09 17:12:38 +0800

    use docker healthcheck for test containers

--- dockerbuild/run-integration-tests.sh
+++ dockerbuild/run-integration-tests.sh
@@ -8,8 +8,4 @@ set -e
 
 cd /data
 
-whenavail "ssp-hub.local"  80 15 echo Hub ready
-whenavail "ssp-idp1.local" 80 20 echo IDP 1 ready
-whenavail "ssp-sp1.local"  80 5 echo SP 1 ready
-
 behat --no-interaction
