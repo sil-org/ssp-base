f8a3c1bb5732219d85593011aca80ade79c27d6a
briskt <3172830+briskt@users.noreply.github.com>
2026-05-20 17:57:28 +0800

    disable the admin module except for test containers

--- development/enable-exampleauth.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-
-sed -i 's@^\( *'\''module\.enable'\'' => \[\)@\1\n        '\''exampleauth'\'' => true,@' /data/vendor/simplesamlphp/simplesamlphp/config/config.php
---
+++ development/idp3-local/config/extra-config.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'module.enable' => [
+        'admin' => true,
+        'exampleauth' => false,
+    ]
+];
