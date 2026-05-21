2a074d71db8ec87957545be58d3c621f09781036
briskt <3172830+briskt@users.noreply.github.com>
2026-05-21 09:29:13 +0800

    remove the core:AdminPassword from the default authsources.php

--- dockerbuild/config/authsources.php
+++ dockerbuild/config/authsources.php
@@ -3,15 +3,6 @@
 use SimpleSAML\Module\silauth\Auth\Source\config\ConfigManager;
 
 $config = [
-
-    // This is a authentication source which handles admin authentication.
-    'admin' => [
-        // The default is to use core:AdminPassword, but it can be replaced with
-        // any authentication source.
-
-        'core:AdminPassword',
-    ],
-
-    // Use SilAuth
+    // Use the SimpleSAML\Module\silauth\Auth\Source\SilAuth Auth Proc (Authentication Processing Filter)
     'silauth' => ConfigManager::getSspConfig(),
 ];
